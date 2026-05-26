e331cf35438468e43d0ceeaac14a58dc17b3fe39
hayatodofreefire <hayat.azam@escola.pr.gov.br>
2026-05-26 18:00:50 -0300

    Delete mobile.js

--- mobile.js
+++
@@ -1,5 +0,0 @@
-const input = require("readline-sync");
-let nome = input.question("digite seu nome: ");
-let idade = number(input.question("digite sua idade: "));
-let cidade = input.question("digite sua cidade: ");
-console.log("Olá! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade}.");
